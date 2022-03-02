f39f74e344124c5a6ac986836e4f07cc5ef07355
Frcomparan <frcomparan@gmail.com>
2022-03-01 18:13:59 -0600

    Refactor update_quality method into some shorter methods
    Co-authored-by: femm34 <femm34@users.noreply.github>
    Co-authored-by: EliasUR <eliasrodriguez2002@hotmail.com>

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,51 +6,45 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def normal_item(item)
+    if item.quality.positive?
+      item.quality = if item.sell_in.positive? || item.quality == 1
+                       item.quality - 1
+                     else
+                       item.quality - 2 
+                     end
+    else
+      item.quality = 0
+    end
+  end
+
+  def backstage_quality(item)
+    if item.quality < 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 11 && item.quality < 50
+      item.quality += 1 if item.sell_in < 6 && item.quality < 50
+    end
+    item.quality = 0 if item.sell_in <= 0
+  end
+
+  def aged_brie_quality(item)
+    if item.quality < 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in <= 0 && item.quality < 50
+    end
+  end
+
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+      case item.name
+      when 'Aged Brie' then aged_brie_quality(item)
+      when 'Backstage passes to a TAFKAL80ETC concert' then backstage_quality(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        normal_item(item)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.sell_in = item.sell_in - 1
     end
   end
 end
@@ -68,6 +62,8 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+
 
 # -------- day 0 --------
 # name, sellIn, quality
